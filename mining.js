f8613cf6928bd8adb284bce008a83210f489180f
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2021-07-26 17:49:03 +0300

    compatibility detection
    Integrated gpus get filtered out

--- mining.js
+++ mining.js
@@ -10,88 +10,98 @@ let spinner;
 
 
 async function downloadFile(url, location, name) {
-    const stream = fs.createWriteStream(location);
-    const request = await https.get(url, function(response) {
-        if(parseInt(response.statusCode) >= 200 && parseInt(response.statusCode) < 300) { 
-            response.pipe(stream);
-            stream.on('finish', function() {
-                stream.close(function(){
-                    spinner.succeed(chalk.bold.green(`Downloaded ${name}`));
-                });
-            });
-        } else {
-            downloadFile(response.headers.location, location, name);
-        }
-    });
+	const stream = fs.createWriteStream(location);
+	const request = await https.get(url, function(response) {
+		if(parseInt(response.statusCode) >= 200 && parseInt(response.statusCode) < 300) { 
+			response.pipe(stream);
+			stream.on('finish', function() {
+				stream.close(function(){
+					spinner.succeed(chalk.bold.green(`Downloaded ${name}`));
+				});
+			});
+		} else {
+			downloadFile(response.headers.location, location, name);
+		}
+	});
 }
 
 async function run() {
-    if (!fs.existsSync("./data/miners")){
-        fs.mkdirSync("./data/miners");
-    }
-    console.clear();
-    console.log(chalk.bold.cyan(`Configure your miner`))
-    spinner = ora("Loading miner list").start();
-    fetch('https://raw.githubusercontent.com/VukkyLtd/SaladBind/main/internal/miners.json?token=ALJSKC4Y47KW2EIR7TCUDFDA722W4')
-        .then(res => res.json())
-        .then(async data => {
-            spinner.stop();
-            let minerList = [];
-            let temp = await si.osInfo()
-            let temp2 = await si.graphics()
-            let userPlatform = temp.platform;
-            let GPUs = [];
-            console.log(temp2.controllers[0].vram)
-            for (let i = 0; i < temp2.controllers.length; i++) {
-                let compatibleAlgos = []
-                for (let j = 0; j < Object.keys(data.algos).length; j++) {
-                    if(temp2.controllers[i].vram > data.algos[Object.keys(data.algos)[i]]) {
-                        compatibleAlgos.push(Object.keys(data.algos)[i])
-                    }
-                }
-                GPUs.push({"algos": compatibleAlgos, "vendor": temp2.controllers[i].vendor});
-            }
-            console.log(GPUs);
-            for (let i = 0; i < Object.keys(data.miners).length; i++) {
-                const minerSupportsOS = data.miners[Object.keys(data.miners)[i]].supported_os.includes(userPlatform)
-                const minerSupportsGPU = GPUs.filter(gpu => data.miners[Object.keys(data.miners)[i]].algos.includes(gpu.algos) && data.miners[Object.keys(data.miners)[i]].supported_gpus.includes(gpu.vendor.toLowerCase())).length > 0
-                if(minerSupportsOS && minerSupportsGPU) {
-                    minerList.push({
-                        name: data.miners[Object.keys(data.miners)[i]].miner,
-                        value: data.miners[Object.keys(data.miners)[i]]
-                    });
-                }
-            }
-            if (minerList.length == 0) {
-                spinner.stop();
-                console.log(chalk.bold.red("No miners are available for your machine D:\nIf you think this is a mistake, talk to us on our Discord server."));
-                setTimeout(() => {
-                    require("./index").menu();
-                }, 6000);
-            } else {
-                const miner = await inquirer.prompt({
-                    type: "list",
-                    name: "miner",
-                    message: "Choose a miner",
-                    choices: minerList
-                });
-                spinner = ora(`Downloading ${miner.miner.miner}-${miner.miner.version}`).start();
-                var downloadURL = miner.miner.download[userPlatform];
-                const fileExtension = path.extname(downloadURL);
-                const fileName = `${miner.miner.miner}-${miner.miner.version}`
-                const fileLocation = `./data/miners/${fileName}${fileExtension}`; 
-                await downloadFile(downloadURL, fileLocation, fileName);
-            }
-        })
-        .catch(err => {
-            spinner.fail(chalk.bold.red(`Could not start a miner. Please try again later.`));
-            console.log(err);
-            setTimeout(() => {
-                require("./index").menu();
-            }, 3500);
-        });
+	if (!fs.existsSync("./data/miners")){
+		fs.mkdirSync("./data/miners");
+	}
+	console.clear();
+	console.log(chalk.bold.cyan(`Configure your miner`))
+	spinner = ora("Loading miner list").start();
+	fetch('https://raw.githubusercontent.com/VukkyLtd/SaladBind/main/internal/miners.json') //fuck you token
+		.then(res => res.json())
+		.then(async data => {
+			spinner.stop();
+			let minerList = [];
+			let temp = await si.osInfo()
+			let temp2 = await si.graphics()
+			let userPlatform = temp.platform;
+			let GPUs = [];  
+			for (let i = 0; i < temp2.controllers.length; i++) {
+				let compatibleAlgos = []
+				for (let j = 0; j < Object.keys(data.algos).length; j++) {
+					if(temp2.controllers[i].vram > data.algos[Object.keys(data.algos)[j]]) {
+						compatibleAlgos.push(Object.keys(data.algos)[j])
+					}
+				}
+				if (compatibleAlgos.length > 0) {
+					GPUs.push({"algos": compatibleAlgos, "vendor": temp2.controllers[i].vendor.toLowerCase()});
+				}
+			}
+			for (let i = 0; i < Object.keys(data.miners).length; i++) {
+				let minerData = data.miners[Object.keys(data.miners)[i]];
+				const minerSupportsOS = minerData.supported_os.includes(userPlatform)
+				const algosSupportsGPU = minerData.algos.filter(algo => GPUs.filter(gpu => gpu.algos.includes(algo)).length > 0).length > 0
+				
+				const minerSupportsGPU = GPUs.filter(gpu => minerData.supported_gpus.includes(gpu.vendor)).length > 0
+				if(minerSupportsOS && minerSupportsGPU && algosSupportsGPU) {
+					if (GPUs.filter(gpu => minerData.algos.filter(algo => gpu.algos.includes(algo)).length > 0).length != GPUs.length) {
+						minerList.push({
+							name: minerData.miner + ` ${chalk.yellow("Not supported by some of your GPUs")}`,
+							value: minerData
+						});
+					} else {
+						minerList.push({
+							name: minerData.miner,
+							value: minerData
+						});
+					}
+				}
+			}
+			if (minerList.length == 0) {
+				spinner.stop();
+				console.log(chalk.bold.red("No miners are available for your machine D:\nIf you think this is a mistake, talk to us on our Discord server."));
+				setTimeout(() => {
+					require("./index").menu();
+				}, 6000);
+			} else {
+				const miner = await inquirer.prompt({
+					type: "list",
+					name: "miner",
+					message: "Choose a miner",
+					choices: minerList
+				});
+				spinner = ora(`Downloading ${miner.miner.miner}-${miner.miner.version}`).start();
+				var downloadURL = miner.miner.download[userPlatform];
+				const fileExtension = path.extname(downloadURL);
+				const fileName = `${miner.miner.miner}-${miner.miner.version}`
+				const fileLocation = `./data/miners/${fileName}${fileExtension}`; 
+				await downloadFile(downloadURL, fileLocation, fileName);
+			}
+		})
+		.catch(err => {
+			spinner.fail(chalk.bold.red(`Could not start a miner. Please try again later.`));
+			console.log(err);
+			setTimeout(() => {
+				require("./index").menu();
+			}, 3500);
+		});
 }
 
 module.exports = {
-    run
+	run
 };
