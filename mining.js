39b7000ef73b632d5a49df88a9fecf33e374ece1
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2021-07-27 16:03:50 +0300

    Extraction with handling of dupe folders, algorithm selection

--- mining.js
+++ mining.js
@@ -8,7 +8,23 @@ const https = require('https');
 const path = require('path');
 const extract = require('extract-zip');
 const tar = require('tar');
+const mv = require('mv'); //what i forogt to save :bukky:
 let spinner;
+
+function moveDupeFolder(folderName) {
+	let folderData = fs.readdirSync(`./data/miners/${folderName}`)
+	if(folderData.length == 1) {
+		mv(`./data/miners/${folderName}/${folderData[0]}`, `./data/miners/${folderName}`, {clobber: false}, function(err) { //oh okers
+			// done. it tried fs.rename first, and then falls back to
+			// piping the source file to the dest file and then unlinking
+			// the source file. (docs lol) lol
+			if(err) {
+				console.log(chalk.bold.red(err));
+				spinner.fail();
+			}
+		});
+	}
+}
 
 async function extractFile(location, folderName, fileExtension) {
     if (!fs.existsSync(`./data/miners/${folderName}`)){
@@ -18,6 +34,7 @@ async function extractFile(location, folderName, fileExtension) {
 		case ".zip":
     		await extract(require('path').resolve(location), { dir: path.resolve(`./data/miners/${folderName}`) });
 			fs.unlinkSync(location);
+			moveDupeFolder(folderName);
 		break;
 		case ".tgz":
 		case ".tar.gz":
@@ -32,6 +49,7 @@ async function extractFile(location, folderName, fileExtension) {
 				})
 			).on("end", () => {
 				fs.unlinkSync(location)
+				moveDupeFolder(folderName);
 			})
 		break;
 	}
@@ -66,25 +84,26 @@ async function run() {
 	console.clear();
 	console.log(chalk.bold.cyan(`Configure your miner`))
 	spinner = ora("Loading miner list").start();
-	fetch('https://raw.githubusercontent.com/VukkyLtd/SaladBind/main/internal/miners.json?token=ALJSKC2PDZZVPSHH55NHY6TA77UV6') //fuck you token
+	fetch('https://raw.githubusercontent.com/VukkyLtd/SaladBind/main/internal/miners.json?token=ALJSKC424AHDPQ7QYTVKBA3BAAAYA') //fuck you token
 		.then(res => res.json())
 		.then(async data => {
 			spinner.stop();
 			let minerList = [];
 			let temp = await si.osInfo()
 			let temp2 = await si.graphics()
-			let userPlatform = "win32" //temp.platform;
+			let userPlatform = temp.platform;
 			let GPUs = [];  
 			for (let i = 0; i < temp2.controllers.length; i++) {
 				let compatibleAlgos = []
 				for (let j = 0; j < Object.keys(data.algos).length; j++) {
-					if(temp2.controllers[i].vram > data.algos[Object.keys(data.algos)[j]]) {
-						compatibleAlgos.push(Object.keys(data.algos)[j])
-					}
-				}
+					if(temp2.controllers[i].vram > data.algos[Object.keys(data.algos)[j]]) { 
+						compatibleAlgos.push(Object.keys(data.algos)[j]) // no, we'd have to flip it around i think
+					} //wait no am i having a stroke so its the amount of supported algos yupp ok very cool!
+				} // 
 				if (compatibleAlgos.length > 0) {
 					GPUs.push({"algos": compatibleAlgos, "vendor": temp2.controllers[i].vendor.toLowerCase()});
 				}
+				GPUs.push({"algos": ["etchash", "kawpow"], "vendor": "nvidia"}); //fake gpu right here (yeah but your real one makes ethash show up) indeed
 			}
 			for (let i = 0; i < Object.keys(data.miners).length; i++) {
 				let minerData = data.miners[Object.keys(data.miners)[i]];
@@ -120,6 +139,11 @@ async function run() {
 				});
 				if(!fs.existsSync(`./data/miners/${miner.miner.miner}-${miner.miner.version}`)) {
 					spinner = ora(`Downloading ${miner.miner.miner}-${miner.miner.version}`).start();
+					let miners = fs.readdirSync("./data/miners");
+					let oldMiners = miners.filter(miner => miner.startsWith(miner.miner.miner));
+					if(oldMiners.length > 0) { //woo! time for pools.json (and more fucking tokens) oh piss
+						oldMiners.forEach(miner => fs.unlinkSync(`./data/miners/${miner}`));
+					}
 					var downloadURL = miner.miner.download[userPlatform];
 					var fileExtension = path.extname(downloadURL); //time for a really hacky solution
 					if (fileExtension == ".gz") {
@@ -130,13 +154,58 @@ async function run() {
 					downloadFile(downloadURL, fileLocation, fileName).then(async () => {
 						spinner = ora(`Extracting ${miner.miner.miner}-${miner.miner.version}`).start();
 						await extractFile(fileLocation, fileName, fileExtension)
+						selectAlgo(miner.miner, GPUs);
 					});
+				} else {
+					selectAlgo(miner.miner, GPUs);
 				}
-				//algoSelection()
 			}
 		})
 		.catch(err => {
 			spinner.fail(chalk.bold.red(`Could not start a miner. Please try again later.`));
+			console.log(err);
+			setTimeout(() => {
+				require("./index").menu();
+			}, 3500);
+		});
+}
+
+async function selectAlgo(minerData, GPUs) {
+	console.clear();
+	console.log(chalk.bold.cyan(`Configure your miner`))
+	let algoList = [];
+	const gpuSupportsAlgo = minerData.algos.filter(algo => GPUs.filter(gpu => gpu.algos.includes(algo)).length > 0)
+	const temptemp = GPUs.filter(gpu => minerData.algos.filter(algo => gpu.algos.includes(algo)).length > 0)
+	for (let i = 0; i < gpuSupportsAlgo.length; i++) { //im having a really bhig stroke
+		let notSupportedByAll;
+		for (let j = 0; j < GPUs.length; j++) {
+			if(!GPUs[j].algos.includes(gpuSupportsAlgo[i])) {
+				notSupportedByAll = true;
+			}
+		}
+		if (notSupportedByAll == true) { // now. the cursed. pool selection and miner running. ok pool selection is only needed if ethash is selected
+			algoList.push({name: `${gpuSupportsAlgo[i]} ${chalk.yellow("(Not supported by some of your GPUs)")}`, value: gpuSupportsAlgo[i]});
+		} else {
+			algoList.push({name: gpuSupportsAlgo[i], value: gpuSupportsAlgo[i]});
+		}
+	} 
+	const algo = await inquirer.prompt({
+		type: "list", 
+		name: "algo",
+		message: "Choose an algorithm",
+		choices: algoList
+	});
+	selectPool(minerData, algo.algo);
+}
+
+async function selectPool(minerData, algo) {
+	spinner = ora("Loading pool list").start();
+	fetch('https://raw.githubusercontent.com/VukkyLtd/SaladBind/main/internal/pools.json?token=ALJSKC424AHDPQ7QYTVKBA3BAAAYA') //fuck you token
+		.then(res => res.json())
+		.then(async poolData => {
+			//just thinking of a "cool" way to do this ok ill push now
+		}).catch(err => { //make this into something i can commit
+			spinner.fail(chalk.bold.red(`Could not select a pool. Please try again later.`));
 			console.log(err);
 			setTimeout(() => {
 				require("./index").menu();
