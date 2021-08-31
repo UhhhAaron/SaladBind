3b4d6d288e8e3508d3de0df3458155c4ee4f9283
Vukky <46850780+Vukkyy@users.noreply.github.com>
2021-08-31 10:27:49 +0200

    Update alfurValidator.js

--- internal/alfurValidator.js
+++ internal/alfurValidator.js
@@ -1,7 +1,7 @@
 const changelog = require("./changelog.json");
 const pjson = require("../package.json");
 
-if(changelog.version == pjson.version) {
-    console.log("Please update internal/changelog.json before pushing a new build!");
+if(changelog.version > pjson.version) {} else {
+    console.log("Please update internal/changelog.json before pushing a new build!\n");
     process.exit(1);
 }
