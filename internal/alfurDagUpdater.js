7a1428ada8cf1ebd374e82f74b1582c836d0ed1a
Vukky <46850780+Vukkyy@users.noreply.github.com>
2021-09-12 13:13:30 +0200

    i sure hope this works lmfao

---
+++ internal/alfurDagUpdater.js
@@ -0,0 +1,23 @@
+const miners = require("./miners.json");
+const fetch = require("node-fetch");
+const fs = require("fs");
+
+// this uses the minerstat "api", kind of bad but it works! :)
+async function getDag(coin) {
+    const params = new URLSearchParams();
+    params.append('coin', coin);
+    const response = await fetch('https://minerstat.com/dag-size-calculator', {method: 'POST', body: params});
+    const woop = await response.json();
+    return woop[2];
+}
+
+async function doTheStuff(){
+    const ethDag = Math.round(await getDag("ETH") * 1000);
+    const etcDag = Math.round(await getDag("ETC") * 1000);
+    const rvnDag = Math.round(await getDag("RVN") * 1000);
+    miners.algos.ethash = ethDag;
+    miners.algos.etchash = etcDag;
+    miners.algos.kawpow = rvnDag;
+    fs.writeFileSync("./internal/miners.json", JSON.stringify(miners, null, 4));
+}
+doTheStuff();
