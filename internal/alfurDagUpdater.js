c276ea363f6c79ba6e28df1df0e5fca1220ecb52
EvadeMaster <93124920+EvadeMaster@users.noreply.github.com>
2022-10-03 21:57:56 +0700

    🛠️ Minor improvements to the DAG Updater

--- internal/alfurDagUpdater.js
+++ internal/alfurDagUpdater.js
@@ -12,14 +12,31 @@ async function getDag(coin) {
 }
 
 async function doTheStuff(){
-    const ethDag = Math.round(await getDag("ETH") * 1000);
+    //const ethDag = Math.round(await getDag("ETH") * 1000);
+    const ethDag = 2147483647;
     const etcDag = Math.round(await getDag("ETC") * 1000);
     const rvnDag = Math.round(await getDag("RVN") * 1000);
     const ergDag = Math.round(await getDag("ERG") * 1000);
+    //miners.algos.ethash = ethDag;
     miners.algos.ethash = ethDag;
     miners.algos.etchash = etcDag;
     miners.algos.kawpow = rvnDag;
     miners.algos.autolykos2 = ergDag;
+    console.log(`
+
+Algorithm overview:
+
+    Ethash      will work when VRAM is higher than   ${ethDag}
+    Etchash     will work when VRAM is higher than   ${etcDag}
+    Kawpow      will work when VRAM is higher than   ${rvnDag}
+    Autolykos2  will work when VRAM is higher than   ${ergDag}
+
+Please check if the result
+
+    `)
     fs.writeFileSync("./internal/miners.json", JSON.stringify(miners, null, 4));
+    console.log(`
+    Update DAG complete!
+    `)
 }
 doTheStuff();
